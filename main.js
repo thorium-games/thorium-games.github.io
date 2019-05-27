45d40f267706b17d4702b29ba7f5cf91351cd2ef
malav-mehta <malav.mehta@hotmail.com>
2019-05-27 13:49:15 -0400

    Started Developing UI

---
+++ main.js
@@ -0,0 +1,25 @@
+let splashDiv = document.getElementById("splashDiv");
+let gameDiv = document.getElementById("gameDiv");
+let aboutDiv = document.getElementById("aboutDiv");
+let teamDiv = document.getElementById("teamDiv");
+let contactDiv = document.getElementById("contactDiv");
+
+function showHome() {
+    splashDiv.scrollIntoView();
+}
+
+function showGame() {
+    gameDiv.scrollIntoView();
+}
+
+function showAbout() {
+    aboutDiv.scrollIntoView();
+}
+
+function showTeam() {
+    teamDiv.scrollIntoView();
+}
+
+function showContact() {
+    contactDiv.scrollIntoView();
+}
