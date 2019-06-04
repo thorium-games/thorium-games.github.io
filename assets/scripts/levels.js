debaacf922ae17a1ecee892c6b2ad7a04f4b33de
malav-mehta <malav.mehta@hotmail.com>
2019-06-04 12:37:59 -0400

    changes

--- assets/scripts/levels.js
+++ assets/scripts/levels.js
@@ -223,9 +223,3 @@ var GAME_LEVELS = [
 #++++..o..o..o..o.......#####################################################
 #############################################################################`
 ];
-
-
-if (typeof module != "undefined" && module.exports && (typeof window == "undefined" || window.exports != exports))
-    module.exports = GAME_LEVELS;
-if (typeof global != "undefined" && !global.GAME_LEVELS)
-    global.GAME_LEVELS = GAME_LEVELS;
