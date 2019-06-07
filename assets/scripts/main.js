3e83cb0b57e8899c23e7461eb3145a16f17fcd49
malav-mehta <malav.mehta@hotmail.com>
2019-06-06 22:34:38 -0400

    commenting js files, updating levels, optimizing code, fixing bugs and typos

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,19 +1,23 @@
+// defining all divs as variables
 var splashDiv = document.getElementById("splash-block");
 var aboutDiv = document.getElementById("about-block");
 var gameDiv = document.getElementById("game-block");
 var teamDiv = document.getElementById("team-block");
 var contactDiv = document.getElementById("contact-block");
 
+// scrolling divs into view when this function is called
 function navigateToDiv(div) {
     div.scrollIntoView();
 }
 
+// keeping track of ip addresses to ban users with inappropriate names
 // var ipAdress;
 
 // $.getJSON('http://gd.geobytes.com/GetCityDetails?callback=?', function(data) {
 //     ipAddress = data.geobytesremoteip;
 // });
 
+// firebase credentials for the server
 var firebaseConfig = {
     apiKey: "AIzaSyBgxqqy4BkOo_ZSZHamiHtxaIOuaHUemz4",
     authDomain: "thorium-games.firebaseapp.com",
@@ -24,8 +28,10 @@ var firebaseConfig = {
     appId: "1:877408314624:web:f6cee9b114752466"
 };
 
+// intializing firebase app and database
 firebase.initializeApp(firebaseConfig);
 
+// variables used for sign in and create account modals/functions
 var username;
 var userPassword;
 var userCount;
@@ -42,12 +48,9 @@ var signInButton = document.getElementById("sign-in-button");
 var createAccountButton = document.getElementById("create-account-button");
 var mainSignIn = document.getElementById("main-sign-in");
 
-ip = Request.UserHostAddress;
-hostname = Request.UserHostName;
-console.log(ip, hostname);
-
+// creating a new user
 function createUser(userId, name, password, currentLevel) {
-    database.ref('players/' + userId).set({
+    database.ref('players/' + userId).set({ // creating a databse reference point and updating its values
         username: name,
         password: password,
         level: currentLevel,
@@ -60,10 +63,12 @@ function createUser(userId, name, password, currentLevel) {
     createAccountButton.innerHTML = "account created!";
 }
 
+// updating user count with new users
 function updateUserCount(count) {
     database.ref('players/count').set(count);
 }
 
+// getting user count to keep track of the # of users
 function getUserCount() {
     return database.ref('players/count').once('value').then(function(snapshot) {
         userCount = (snapshot.val() && snapshot.val());
@@ -72,27 +77,30 @@ function getUserCount() {
 
 getUserCount();
 
+// collecting user input for authentication
 function getInputForSignIn() {
     username = userId.value;
     password = userInputPassword.value;
-    if (username != "" && password != "" && username.length <= 12) {
+    if (username != "" && password != "" && username.length <= 12) { // validating input
         signIn(username, password);
     } else {
         alert("You may not leave any fields blank, and you user id must be less than or equal 12 characters in length");
     }
 }
 
+// collecting new user values from create account modal
 function getInputForNewUser() {
     username = newUserId.value;
     password = newUserPassword.value;
     name = nameInput.value;
-    if (username != "" && password != "" && name != "" && username.length <= 12 && name.length <= 12) {
+    if (username != "" && password != "" && name != "" && username.length <= 12 && name.length <= 12) { // validating input
         createUser(username, name, password, 0);
     } else {
         alert("You may not leave any fields blank, and you username/user id must be less than or equal 12 characters in length");
     }
 }
 
+// showing a modal and binding "enter" key as submit button
 function showModal(modal) {
     modal.style.display = "inline-block";
     window.onkeypress = function(event) {
@@ -108,16 +116,20 @@ function showModal(modal) {
     }
 }
 
+
+// closing a modal
 function cancelModal(modal) {
     modal.style.display = "none";
 }
 
+// sign in function gets the password and authorizes the user
 function signIn(userId) {
     getPassword(userId);
     signInButton.innerHTML = "<span><img src='assets/images/misc/loading.gif' height='15px'></span>"
     setTimeout(authPassword, 500);
 }
 
+// password is autorised by matching inputted password to database password
 function authPassword() {
     if (password == userPassword) {
         loggedIn = true;
@@ -131,19 +143,21 @@ function authPassword() {
     signInButton.innerHTML = "sign in &rarr;";
 }
 
+// get the user's information from the database
 function getPassword(userId) {
-    return database.ref('/players/' + userId).once('value').then(function(snapshot) {
-        userPassword = (snapshot.val() && snapshot.val().password);
-        name = (snapshot.val() && snapshot.val().username);
-        username = userId;
-        currentLevel = (snapshot.val() && snapshot.val().level);
+    return database.ref('/players/' + userId).once('value').then(function(snapshot) { // accessing the user's databse entry
+        userPassword = (snapshot.val() && snapshot.val().password); // getting the value of the user's password
+        name = (snapshot.val() && snapshot.val().username); // getting the user's name
+        username = userId; // getting the username//userid
+        currentLevel = (snapshot.val() && snapshot.val().level); // getting the user's current level
         if (currentLevel > globalLevel) {
-            globalLevel = currentLevel;
+            globalLevel = currentLevel; // updating globallevel variable form games.js
         }
-        currentDeaths = (snapshot.val() && snapshot.val().deaths);
+        currentDeaths = (snapshot.val() && snapshot.val().deaths); // getting user's current death count
     });
 }
 
+// get the leaderboard query from the database
 function getLeaderboard() {
     return database.ref('players/').orderByChild('ratio').then(function(snapshot) {
         var leaderboard = (snapshot.val() && snapshot.val());
@@ -151,33 +165,40 @@ function getLeaderboard() {
     })
 }
 
+
+// function used to update leaderboard
 function updateLeaderboard() {
-    const query = database.ref('/players/').orderByChild('ratio').limitToLast(5)
-    var count = 5;
-    query.once('value', function(snapshot) {
-        snapshot.forEach(function(childSnapshot) {
-            var childRatio = (childSnapshot.val() && childSnapshot.val().ratio)
-            var childName = (childSnapshot.val() && childSnapshot.val().username);
-            document.getElementById(`posName${count}`).innerHTML = childName;
-            document.getElementById(`ratio${count}`).innerHTML = childRatio;
-            count--;
+    const query = database.ref('/players/').orderByChild('ratio').limitToLast(5) // using a databse query to find top 5 users
+    var count = 5; // the number of users to load
+    query.once('value', function(snapshot) { // finding values for each user using the query
+        snapshot.forEach(function(childSnapshot) { // for each "snapshot" value, the following lines are executed
+            var childRatio = (childSnapshot.val() && childSnapshot.val().ratio) // the ratio is retrieved
+            var childName = (childSnapshot.val() && childSnapshot.val().username); // the user name is retrieved
+            document.getElementById(`posName${count}`).innerHTML = childName; // the name is updated on leaderboard
+            document.getElementById(`ratio${count}`).innerHTML = childRatio; // the ratio is updated on leaderboard
+            count--; // the count diminishes for the dom updating lines above
         });
     });
 }
 
-updateLeaderboard();
+updateLeaderboard(); // leaderboard updated on page load
 
 function saveProgress() {
     currentDeaths += deaths;
     deaths = 0;
     currentLevel = globalLevel - 1;
+
+    // adding one death to ensure there are now NaN values in database
     if (currentDeaths == 0) {
         currentDeaths++;
     }
+
+    // checking to make sure that the user has not cheated by adding to ratio through console, and resetting progress if so
     if ((Math.round(((currentLevel / (currentDeaths)) * 100)) / 100 > gameLevels.length) || ((Math.round((currentLevel / (currentDeaths)) * 100)) / 100 > currentLevel)) {
         resetProgress();
         return null;
     }
+    // changing variables in the location of the users database entry (in JSON format)
     database.ref('players/' + username).set({
         username: name,
         password: password,
@@ -185,10 +206,10 @@ function saveProgress() {
         deaths: currentDeaths,
         ratio: (Math.round((currentLevel / (currentDeaths)) * 100)) / 100
     })
-
     updateLeaderboard();
 }
 
+// resetting user progress on demand by resetting variables
 function resetProgress() {
     currentDeaths = 0;
     currentLevel = 0;
@@ -197,6 +218,7 @@ function resetProgress() {
     saveProgress();
 }
 
+// loading the game for users if they have logged in
 function loadGame() {
     if (loggedIn) {
         document.getElementById("actual-game-div").style.display = "inline-block";
@@ -209,7 +231,9 @@ function loadGame() {
     }
 }
 
-setInterval(saveProgress, 5000);
+setInterval(saveProgress, 5000); // progress regularly saved every 5 seconds
+
+// functions use to turn music on and off, and to change the attributes of the button toggling the music state
 
 function musicOff() {
     bgAudio.pause();
@@ -221,4 +245,20 @@ function musicOn() {
     bgAudio.play();
     document.getElementById("music-switch").innerHTML = "music off";
     document.getElementById("music-switch").setAttribute("onclick", "musicOff()");
+}
+
+// disabling all keyboard variables allowing access to console for database manipulation;
+document.onkeydown = function(e) {
+    if (event.keyCode == 123) {
+        return false;
+    }
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+        return false;
+    }
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+        return false;
+    }
+    if (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+        return false;
+    }
 }
